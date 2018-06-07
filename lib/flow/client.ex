d761c10f929ccb132ac617f820ccb40dc6aa8f2d
Lukas Rieder <l.rieder@gmail.com>
2018-06-07 16:54:01 +0200

    add error handling to Flow.Client
    * TODO: fix this one issue with reply_to reference registration

--- lib/flow/client.ex
+++ lib/flow/client.ex
@@ -1,7 +1,22 @@
 defmodule Flow.Client do
   use GenServer
 
+  require Logger
+
+  defmodule TimeoutError do
+    defexception [:message]
+  end
+
+  defmodule Error do
+    defexception [:message, :error]
+  end
+
   alias Flow.Pattern.Controller
+
+  def start(pid_or_name, opts \\ []) do
+    {:ok, pid} = start_link(pid_or_name, opts)
+    pid
+  end
 
   def start_link(pid_or_name, opts \\ []) do
     GenServer.start_link(__MODULE__, pid_or_name, opts)
@@ -12,7 +27,22 @@ defmodule Flow.Client do
   end
 
   def call(pid, value, timeout \\ :infinity) do
-    GenServer.call(pid, value, timeout)
+    GenServer.call(pid, {:call, value, timeout}, :infinity)
+  rescue
+    error ->
+      {:error, error}
+  end
+
+  def call!(pid, value, timeout \\ :infinity) do
+    case GenServer.call(pid, {:call, value, timeout}, :infinity) do
+      {:ok, value} -> value
+      {:error, %TimeoutError{} = e} -> raise e
+      {:error, %Error{} = e} -> raise e
+      error ->
+        raise Error,
+          message: "unexpected response received",
+          error: error
+    end
   end
 
   def push(pid, value) do
@@ -42,27 +72,37 @@ defmodule Flow.Client do
     {:reply, value, pid_or_name}
   end
 
-  def handle_call(value, _from, pid_or_name) do
+  def handle_call({:call, value, timeout}, _from, pid_or_name) do
     [producer, consumer] = Controller.get_stages(pid_or_name, [:IN, :OUT])
     reply_to = self()
     ip = Flow.IP.new(value, reply_to: reply_to)
     monitor = Process.monitor(consumer)
     GenStage.cast(producer, ip)
-    value = await_response(reply_to, monitor)
-    {:reply, value, pid_or_name}
+    resp = await_response(reply_to, monitor, timeout)
+    {:reply, resp, pid_or_name}
   end
 
-  def await_response(pid, monitor) do
+  def await_response(pid, monitor, timeout) do
     receive do
       %Flow.IP{value: value, reply_to: ^pid} ->
         Process.demonitor(monitor)
-        value
+        {:ok, value}
+
       {:DOWN, ^monitor, _, _, _reason} ->
-        # TODO: Error, consumer went down
-        nil
+        {:error, %Error{message: "consumer went down while waiting for return on OUT"}}
+
       _ ->
-        await_response(pid, monitor)
+        await_response(pid, monitor, timeout)
+
+    after
+      timeout ->
+        {:error, %TimeoutError{message: "timeout waiting for return on OUT"}}
     end
+  end
+
+  def handle_info(msg, pid_or_name) do
+    Logger.info("#{__MODULE__} received an unexpected message: #{inspect msg}")
+    {:noreply, pid_or_name}
   end
 
 end
--- test/flow/client_test.exs
+++ test/flow/client_test.exs
@@ -12,6 +12,7 @@ defmodule Flow.ClientTest do
         transform: %Block{fun: fn
           "foo" -> "bar"
           "zig" -> "zag"
+          fun when is_function(fun) -> fun.()
         end}
       },
       subscriptions: [
@@ -23,8 +24,26 @@ defmodule Flow.ClientTest do
     :ok
   end
 
+  test "#call! pushes a messages on IN and waits for the response on OUT" do
+    assert "bar" == Client.call!(:our_client, "foo")
+  end
+
   test "#call pushes a messages on IN and waits for the response on OUT" do
-    assert "bar" == Client.call(:our_client, "foo")
+    assert {:ok, "bar"} == Client.call(:our_client, "foo")
+  end
+
+  test "#call with timeout" do
+    assert false
+    # TODO:
+    #
+    #   The client awaits messages with its own PID set to reply_to.
+    #
+    #   In fact, it must await messages for itself
+    #   AND messages that are tagged uniquely with that :call request.
+    #
+    assert {
+      :error, %Client.TimeoutError{}
+    } = Client.call(:our_client, fn -> :timer.sleep(1000) end, 50)
   end
 
   # test "#pull demands a messages from OUT of the pattern" do
