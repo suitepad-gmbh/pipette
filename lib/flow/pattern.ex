cbcb15ab970ad22f3d6016350e3627afe09553bc
Paul Spieker <p.spieker@duenos.de>
2018-06-12 09:28:17 +0200

    [BUGFIX] Starting the pattern fails
    * The #start_link and 3child_spec methods need a parameter `opts`
    * Pattern.Controller child_specs need to wrap the name option in a second parameter

--- lib/flow/pattern.ex
+++ lib/flow/pattern.ex
@@ -1,6 +1,6 @@
 defmodule Flow.Pattern do
 
-  defmacro __using__(opts \\ []) do
+  defmacro __using__(_opts \\ []) do
     quote do
       def process_name, do: __MODULE__
 
@@ -16,9 +16,9 @@ defmodule Flow.Pattern do
         })
       end
 
-      def start_link(), do: Flow.Pattern.Controller.start_link(__MODULE__.pattern())
+      def start_link(_opts \\ []), do: Flow.Pattern.Controller.start_link(__MODULE__.pattern())
 
-      def child_spec(), do: Flow.Pattern.Controller.child_spec(__MODULE__.pattern())
+      def child_spec(_opts \\ []), do: Flow.Pattern.Controller.child_spec(__MODULE__.pattern())
 
       defoverridable [process_name: 0, blocks: 0, subscriptions: 0]
     end
@@ -31,7 +31,7 @@ defmodule Flow.Pattern do
     blocks: %{},
     subscriptions: []
 
-  def new(%{id: id, blocks: blocks, subscriptions: subscriptions} = args) do
+  def new(%{id: id, blocks: blocks, subscriptions: subscriptions}) do
     blocks = blocks
              |> Map.put(:IN, blocks[:IN] || %Block.PushProducer{})
              |> Map.put(:OUT, blocks[:OUT] || %Block.Consumer{})
@@ -49,4 +49,3 @@ defmodule Flow.Pattern do
   end
 
 end
-
